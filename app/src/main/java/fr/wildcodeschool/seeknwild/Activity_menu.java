1dde97bb5f2bb12f6791c00be72aabdcc27ffd0e
Joséphine Godard <josephine.godard@gmail.com>
2019-05-22 13:45:47 +0200

    V1.1 NavigationDrawer

--- app/src/main/java/fr/wildcodeschool/seeknwild/Activity_menu.java
+++ app/src/main/java/fr/wildcodeschool/seeknwild/Activity_menu.java
@@ -1,10 +1,14 @@
 package fr.wildcodeschool.seeknwild;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 public class Activity_menu extends AppCompatActivity {
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +17,24 @@ public class Activity_menu extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        //TODO Changer le nom et le "bonjour" en plusieurs langues. Mettre "me deconnecter" tout en bas.
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
     }
 }
