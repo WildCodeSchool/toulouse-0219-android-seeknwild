735f2e13bda247525dfe702ced7ab95bb3a69c1f
Joséphine Godard <josephine.godard@gmail.com>
2019-05-22 12:01:41 +0200

    nav_header + activity_menu

---
+++ app/src/main/java/fr/wildcodeschool/seeknwild/Activity_menu.java
@@ -0,0 +1,13 @@
+package fr.wildcodeschool.seeknwild;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
