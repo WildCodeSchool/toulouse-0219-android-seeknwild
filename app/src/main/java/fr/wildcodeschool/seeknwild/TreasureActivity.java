96959155da209182e91d7d7a58f3d1d81816141b
cedric <casas.cedric@gmail.com>
2019-05-22 11:24:42 +0200

    maps + clé API

--- app/src/main/java/fr/wildcodeschool/seeknwild/TreasureActivity.java
+++ app/src/main/java/fr/wildcodeschool/seeknwild/TreasureActivity.java
@@ -2,10 +2,10 @@ package fr.wildcodeschool.seeknwild;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.Toast;
 
 public class TreasureActivity extends AppCompatActivity {
@@ -19,9 +19,9 @@ public class TreasureActivity extends AppCompatActivity {
 
     }
 
-    public void checkLocationPermission () {
+    public void checkLocationPermission() {
         if (ContextCompat.checkSelfPermission(TreasureActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
-            != PackageManager.PERMISSION_GRANTED) {
+                != PackageManager.PERMISSION_GRANTED) {
             // Permission granted : ask for it
             ActivityCompat.requestPermissions(TreasureActivity.this,
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
