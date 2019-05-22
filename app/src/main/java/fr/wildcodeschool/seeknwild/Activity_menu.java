c3c0eafaa82e0fe0c8aafa561516d9807e1c9b0c
Joséphine Godard <josephine.godard@gmail.com>
2019-05-22 13:14:38 +0200

    V1 NavigationDrawer

--- app/src/main/java/fr/wildcodeschool/seeknwild/Activity_menu.java
+++ app/src/main/java/fr/wildcodeschool/seeknwild/Activity_menu.java
@@ -2,6 +2,7 @@ package fr.wildcodeschool.seeknwild;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class Activity_menu extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class Activity_menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
