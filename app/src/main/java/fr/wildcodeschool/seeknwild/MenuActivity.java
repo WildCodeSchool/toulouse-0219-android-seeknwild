1a563025629cfe8bd75fef67153586d2b9948c60
Joséphine Godard <josephine.godard@gmail.com>
2019-05-22 23:28:53 +0200

    Fragments de la navigation drawer

--- app/src/main/java/fr/wildcodeschool/seeknwild/MenuActivity.java
+++ app/src/main/java/fr/wildcodeschool/seeknwild/MenuActivity.java
@@ -1,13 +1,16 @@
 package fr.wildcodeschool.seeknwild;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
 
     @Override
@@ -17,14 +20,37 @@ public class MenuActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        //TODO Changer le nom et le "bonjour" en plusieurs langues. Mettre "me deconnecter" tout en bas.
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.choisi_une_aventures:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ChoisiUneAventureFragment()).commit();
+                break;
+            case R.id.edite_tes_aventures:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new EditeTesAventuresFragment()).commit();
+                break;
+            case R.id.voir_ta_galerie:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new VoirTaGalerieFragment()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
@@ -35,4 +61,5 @@ public class MenuActivity extends AppCompatActivity {
             super.onBackPressed();
         }
     }
+
 }
