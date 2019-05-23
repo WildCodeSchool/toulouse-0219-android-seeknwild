d769f0ede32698935af85fa368e8761a512dbdd6
Joséphine Godard <josephine.godard@gmail.com>
2019-05-24 00:57:53 +0200

    V1 pages tutoriels

---
+++ app/src/main/java/fr/wildcodeschool/seeknwild/TutorielActivity.java
@@ -0,0 +1,13 @@
+package fr.wildcodeschool.seeknwild;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TutorielActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutoriel);
+    }
+}
