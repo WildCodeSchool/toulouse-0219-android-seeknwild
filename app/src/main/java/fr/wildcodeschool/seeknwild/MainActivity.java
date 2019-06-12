5a33a8d0857b2ebb6d76641e127e28ba776eec6d
Episkey <paul.beziat66@gmail.com>
2019-06-12 10:19:13 +0200

    Finalisation compte utilisateur

--- app/src/main/java/fr/wildcodeschool/seeknwild/MainActivity.java
+++ app/src/main/java/fr/wildcodeschool/seeknwild/MainActivity.java
@@ -1,8 +1,8 @@
 package fr.wildcodeschool.seeknwild;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -50,6 +50,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, StartAdventureActivity.class));
+            }
+        });
+
+        Button btConect = findViewById(R.id.btAccount);
+        btConect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CreateAccountActivity.class));
             }
         });
     }
--- app/src/main/java/fr/wildcodeschool/seeknwild/SignInActivity.java
+++ app/src/main/java/fr/wildcodeschool/seeknwild/SignInActivity.java
@@ -1,10 +1,10 @@
 package fr.wildcodeschool.seeknwild;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -17,10 +17,10 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class SignIn extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
     private static final String TAG = "EmailPassword";
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,22 +38,21 @@ public class SignIn extends AppCompatActivity {
                 signIn(etLogin.getText().toString(), etPassword.getText().toString());
             }
         });
-
     }
 
     private void signIn(String email, String password) {
         final ConstraintLayout singInL = findViewById(R.id.signIn_layout);
-        mAuth.signInWithEmailAndPassword(email, password)
+        mAuth.signInWithEmailAndPassword(email.trim(), password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             FirebaseUser user = mAuth.getCurrentUser();
-                            Intent goToMapsActivity = new Intent(SignIn.this, HomeActivity.class);
-                            startActivity(goToMapsActivity);
+                            Intent goTo = new Intent(SignInActivity.this, HomeActivity.class);
+                            startActivity(goTo);
                         } else {
                             Log.w(TAG, getString(R.string.failure), task.getException());
-                            Toast.makeText(SignIn.this, getString(R.string.failure), Toast.LENGTH_SHORT).show();
+                            Toast.makeText(SignInActivity.this, getString(R.string.failure), Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
