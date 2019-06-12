231c69d104868358ef7641ac5fb8601f02e7c3f0
cedric <casas.cedric@gmail.com>
2019-06-12 10:47:22 +0200

    ajout de l'appareil photo

--- app/src/main/java/fr/wildcodeschool/seeknwild/TreasureAdventureMapsActivity.java
+++ app/src/main/java/fr/wildcodeschool/seeknwild/TreasureAdventureMapsActivity.java
@@ -7,12 +7,15 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -30,6 +33,11 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class TreasureAdventureMapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -55,12 +63,56 @@ public class TreasureAdventureMapsActivity extends FragmentActivity implements O
         actionFloattingButton();
     }
 
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imgFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imgFileName, ".jpg", storageDir);
+        return image;
+    }
+    // chemin de la photo dans le téléphone
+    private Uri mFileUri = null;
+
+    private void dispatchTakePictureIntent() {
+        // ouvrir l'application de prise de photo
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // lors de la validation de la photo
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // créer le fichier contenant la photo
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException e) {
+                // TODO : gérer l'erreur
+            }
+
+            if (photoFile != null) {
+                // récupèrer le chemin de la photo
+                mFileUri = FileProvider.getUriForFile(this,
+                        "fr.wildcodeschool.seeknwild.fileprovider",
+                        photoFile);
+                // déclenche l'appel de onActivityResult
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, mFileUri);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        ImageView ivRecupPic = findViewById(R.id.ivPic);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            ivRecupPic.setImageURI(mFileUri);
+        }
+    }
+
     private void actionFloattingButton() {
         FloatingActionButton floatBtTakePicTreasure = findViewById(R.id.fbTakePicTreasure);
         floatBtTakePicTreasure.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                dispatchTakePictureIntent();
             }
         });
 
