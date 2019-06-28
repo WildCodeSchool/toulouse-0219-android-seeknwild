5266adcd27b7df00bb41a2e30c51b2bec263c40c
Episkey <paul.beziat66@gmail.com>
2019-06-28 11:59:38 +0200

    Recycle view + accès à modification aventure

--- app/src/main/java/fr/wildcodeschool/seeknwild/adapter/CreateHomeAdapter.java
+++ app/src/main/java/fr/wildcodeschool/seeknwild/adapter/CreateHomeAdapter.java
@@ -2,13 +2,11 @@ package fr.wildcodeschool.seeknwild.adapter;
 
 import android.content.Context;
 import android.content.Intent;
-import android.media.Image;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
