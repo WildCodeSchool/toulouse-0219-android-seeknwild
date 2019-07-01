173272c10e642b7644e7eb7e115c70e961afe568
Joséphine Godard <josephine.godard@gmail.com>
2019-07-01 10:25:01 +0200

    Ajout de la notation

--- app/src/main/java/fr/wildcodeschool/seeknwild/activity/NoteActivity.java
+++ app/src/main/java/fr/wildcodeschool/seeknwild/activity/NoteActivity.java
@@ -1,10 +1,14 @@
 package fr.wildcodeschool.seeknwild.activity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.LayerDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RatingBar;
 
 import fr.wildcodeschool.seeknwild.R;
 
@@ -14,6 +18,10 @@ public class NoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
+
+        RatingBar ratingBar = (RatingBar) findViewById(R.id.rbStar);
+        LayerDrawable stars = (LayerDrawable) ratingBar.getProgressDrawable();
+        stars.getDrawable(2).setColorFilter(Color.YELLOW, PorterDuff.Mode.SRC_ATOP);
 
         Button btContinued = findViewById(R.id.btContinue);
         btContinued.setOnClickListener(new View.OnClickListener() {
